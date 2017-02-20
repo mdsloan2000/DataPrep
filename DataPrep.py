a8b358564768303d2bef54e72039f82673e3aab3
Michael Sloan <mdsloan2000@gmail.com>
2017-02-20 11:48:26 -0500

    Set up template and add data file energy.csv

---
+++ DataPrep.py
@@ -0,0 +1,34 @@
+#*******************************DataPrep.py**********************************
+#
+#  Assignment:      Week Five Assignment
+#  Course:          CPSC 51100
+#  Student:         Michael D Sloan
+#  Date Created:    20-FEB-2017
+#  Date Due:        26-FEB-2017
+# 
+#*******************************************************************************
+
+
+#Imports and global declarations
+
+import pandas as pd
+from pandas import DataFrame, Series #Only needed dataframe.
+
+
+
+                
+def main():
+    #Local Declarations for Main
+    
+    output_header = "\n51100, Spring 2017\n"\
+                + "Name:  Mike Sloan\n"\
+                + "PROGRAMMING ASSIGNMENT #5"
+    
+    print output_header
+    
+#Executes Main Function
+
+if __name__ == "__main__":
+    main()
+        #Loops through and counts the total rating values
+    
