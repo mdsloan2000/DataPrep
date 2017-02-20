7b7cc88ae064389d42a58e2740e0a3256ccb1d08
Michael Sloan <mdsloan2000@gmail.com>
2017-02-20 14:34:38 -0500

    Update data load and drop consolidated rows.

--- DataPrep.py
+++ DataPrep.py
@@ -14,17 +14,26 @@
 import pandas as pd
 from pandas import DataFrame, Series #Only needed dataframe.
 
-
-
-                
+#testing variables
+              
 def main():
     #Local Declarations for Main
+    data_file = "energy.csv"
+    energy_data = DataFrame()
     
     output_header = "\n51100, Spring 2017\n"\
                 + "Name:  Mike Sloan\n"\
                 + "PROGRAMMING ASSIGNMENT #5"
+                
+    #Import CSV to a Dataframe and create original copy in memory
+    energy_data = pd.read_csv(data_file,index_col=0,na_values='..')
+    original_data = energy_data.copy(deep=True)
     
+    energy_data.drop(['EU27total','OECDtotal','World'])
+            
     print output_header
+  
+    
     
 #Executes Main Function
 
